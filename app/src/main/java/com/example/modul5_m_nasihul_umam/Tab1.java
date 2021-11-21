5ec0cb724d39564fefee2fe7ae232c5180a7c97d
nasihulumam17 <nasihulumam15@gmail.com>
2021-11-21 22:09:52 +0700

    first commit

---
+++ app/src/main/java/com/example/modul5_m_nasihul_umam/Tab1.java
@@ -0,0 +1,12 @@
+package com.example.modul5_m_nasihul_umam;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tab1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab1);
+    }
+}
